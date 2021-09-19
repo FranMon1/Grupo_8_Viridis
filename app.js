1f8e1456cc64baff78e5204afba23c3933368efe
cecbourdillon <cecbourdillon@gmail.com>
2021-09-18 23:07:46 -0300

    Primer boceto detalle de producto

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const path= require('path');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, ()=> {
+    console.log('Estamos corriendo en el puerto 3000')
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/index.html'));
@@ -11,7 +16,8 @@ app.get('/cart', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/cart.html'));
 });
 
-app.listen(3000, ()=>
-    console.log('Estamos corriendo en el puerto 3000\nhttp://localhost:3000'));
+
+
+
 
 
