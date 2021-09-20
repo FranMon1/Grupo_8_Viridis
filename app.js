c2ff58a5f363a5096d7fdfefb835d22b23e0186c
FranMon1 <fran.montero95@gmail.com>
2021-09-20 19:34:14 -0300

    Agrego login y register

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const app = express();
 const path= require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
 });
 
 app.get('/cart', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/cart.html'));
 });
 
+app.get('/detail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/detail.html'));
+});
+
 app.listen(3000, ()=>
     console.log('Estamos corriendo en el puerto 3000\nhttp://localhost:3000'));
-
 
