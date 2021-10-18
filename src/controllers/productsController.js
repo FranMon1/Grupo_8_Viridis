fc6a34cbd6cc4d364034ab2e0c041e3d1e7d0a36
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2021-10-18 17:53:41 -0300

    Carpeta Data con Json de productos, funcionamiento de rutas y controllador de productos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+let archivoProductos = fs.readFileSync(path.resolve(__dirname, '../data/products.json'), 'utf-8');
+let productos = JSON.parse(archivoProductos)
+
+const newProductId = function (){
+    let ultimo = 0;
+    productos.forEach(product => {
+    if (product.id > ultimo) {
+        ultimo = product.id
+    }
+})
+return ultimo + 1;
+
+}
+
+
+
 let productsController = {
     product: (req, res) =>{
       return res.render("products/product");
@@ -20,9 +39,30 @@ let productsController = {
     },
     inventory: function (req,res) {
         return res.render('products/inventory')
+    },
+    store: function (req, res, next) {
+
+        
+
+        if (req.file !== undefined) {
+
+            let newProduct = {
+
+               id: newProductId(),
+                ...req.body,
+            }
+
+            newProduct.productimg = req.file.filename
+
+            productos.push(newProduct);
+
+            let JsonDeProductos = JSON.stringify(productos, null, 4);
+            fs.writeFileSync(path.resolve(__dirname, "../data/products.json"), JsonDeProductos)
+        } else {
+            return
+        }
+        res.redirect('/')
     }
 };
-
-
 
 module.exports = productsController;
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,17 +2,36 @@ const express = require('express');
 const path= require('path');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
+const multer = require('multer');
+
+/* Config of multer */
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+    let products = path.join(__dirname, '../../public/images/products')
+        cb(null, products)
+
+    },
+    filename: function (req, file, callback) {
+        let newProductName = 'product-' + Date.now() + path.extname(file.originalname);
+        callback(null, newProductName)
+    }
+})
+
+const uploadProductImg = multer({ storage });
+
+
+// Rutas
+
 
 router.get('/product', productsController.product);
-
 router.get('/cart', productsController.cart);
-
 router.get('/detail', productsController.detail);
 
 router.get('/create', productsController.create);
+router.post('/create',uploadProductImg.single('productimg'), productsController.store);
 
 router.get ('/inventory', productsController.inventory);
-
 router.get ('/edit', productsController.edit);
 
 
