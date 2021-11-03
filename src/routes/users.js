f274870a54f5b56c716bc98c45b534def6b3e9f4
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2021-11-03 19:10:54 -0300

    Preparando para el sprint5

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
 const usersController = require('../controllers/usersController');
 
 
+// Login
+
+
 router.get('/login', usersController.login);
+
+
+// Registro
 
 router.get('/register', usersController.register);
 
