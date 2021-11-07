a23e46bfcf9d52ec3c21d9cc0802a3083dd61dcf
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2021-11-07 17:40:50 -0600

    Ajustes a usersController

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,13 @@
+const fs = require('fs')
+const express = require('express');
+const path = require('path');
+
 const User = require('../models/User.js');
-const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator');
+const { encode } = require('punycode');
+
+const jsonDeUsuarios = fs.readFileSync(path.resolve(__dirname, '..data/users.json'), 'utf-8')
+const usuarios = JSON.parse(jsonDeUsuarios);
 
 
 let usersController = {
@@ -12,7 +20,7 @@ let usersController = {
     },
     inside: function(){
       let validations = validationResult.req
-      
+
     }
 };
 
